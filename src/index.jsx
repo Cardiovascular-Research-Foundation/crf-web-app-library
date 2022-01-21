9eb9af29ec196a656dbb519944bd2f83257d9a26
Greg Simsic <greg@gregsimsic.com>
2022-01-21 17:57:44 -0500

    Exports

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,4 @@
 import { Title, PageTitle, SectionTitle } from "./components/text/Titles"
-import Guard from "./components/auth/Guard"
-import CenterLayout from "./components/layouts/CenterLayout"
-import PlainLayout from "./components/layouts/PlainLayout"
-import StandardLayout from "./components/layouts/StandardLayout"
-import Menu from "./components/nav/Menu"
-import UserMenu from "./components/nav/UserMenu"
 import {
     AppConfigProvider,
     useAppConfig,
@@ -12,14 +6,23 @@ import {
 import Link from "./components/Link"
 import { createMergedMuiTheme } from "./style/util"
 import backendTheme from "./style/backendTheme"
+import Guard from "./components/auth/Guard"
+import CenterLayout from "./components/layouts/CenterLayout"
+import PlainLayout from "./components/layouts/PlainLayout"
+import StandardLayout from "./components/layouts/StandardLayout"
+import Menu from "./components/nav/Menu"
+import UserMenu from "./components/nav/UserMenu"
 import Section from "./components/structure/Section"
 import Spinner from "./components/feedback/Spinner"
+import Standard404 from "./components/feedback/Standard404"
+import SignIn from "./components/auth/SignIn"
 
 export {
     Title,
     PageTitle,
     SectionTitle,
     Guard,
+    SignIn,
     CenterLayout,
     PlainLayout,
     StandardLayout,
@@ -32,4 +35,5 @@ export {
     backendTheme,
     Section,
     Spinner,
+    Standard404,
 }
