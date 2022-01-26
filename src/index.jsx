c741538cdf7b5492e3a35c05e1c9d0aa0f7e7315
Greg Simsic <greg@gregsimsic.com>
2022-01-26 12:17:27 -0500

    Reorganize exports

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,37 @@
-export * from "./components/text/Titles"
-export * from "./components/providers/AppConfigProvider"
-export * from "./style/util"
+// Layout
 export { default as CenterLayout } from "./components/layouts/CenterLayout"
 export { default as PlainLayout } from "./components/layouts/PlainLayout"
 export { default as StandardLayout } from "./components/layouts/StandardLayout"
+export { default as DocHead } from "./components/layouts/DocHead"
+
+// Structure
+export { default as Section } from "./components/structure/Section"
+
+// Pages
+export { default as Standard404 } from "./components/pages/Standard404"
+
+// Auth
 export { default as Guard } from "./components/auth/Guard"
+export { default as SignIn } from "./components/auth/SignIn"
+
+// Providers
+export * from "./components/providers/AppConfigProvider"
+
+// Navigation
 export { default as Menu } from "./components/nav/Menu"
 export { default as UserMenu } from "./components/nav/UserMenu"
-export { default as Section } from "./components/structure/Section"
-export { default as SignIn } from "./components/auth/SignIn"
-export { default as Spinner } from "./components/feedback/Spinner"
-export { default as Standard404 } from "./components/feedback/Standard404"
-export { default as Link } from "./components/Link"
+
+// Styles
 export { default as backendTheme } from "./style/backendTheme"
+export * from "./style/util"
+
+// Text
+export * from "./components/text/Titles"
+export { default as Link } from "./components/Link"
+
+// Feedback
+export { default as Spinner } from "./components/feedback/Spinner"
+
+// Graphics
+export { default as CrfHeart } from "./components/graphics/CrfHeart"
+export { default as CrfLogo } from "./components/graphics/CrfLogo"
