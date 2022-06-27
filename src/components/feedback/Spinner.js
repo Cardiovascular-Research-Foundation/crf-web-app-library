6a2040944fc611e00dcc2ec62b2f1148389c4b8a
Greg Simsic <greg@gregsimsic.com>
2022-06-27 15:35:54 -0400

    Adds options to the Spinner component

--- src/components/feedback/Spinner.js
+++ src/components/feedback/Spinner.js
@@ -1,19 +1,27 @@
 import { Stack, CircularProgress, Typography } from "@mui/material"
 
-const Spinner = ({ label, position = "center", color = "primary" }) => {
+const Spinner = ({ variant = "default", label, position = "center", color = "primary", size }) => {
     const resolvedColor = color[0] === "#" ? { sx: { color: color } } : { color }
+    const resolvedSize = size ? size : variant === "inline" ? 20 : 40
 
     return (
         <Stack
-            direction="column"
+            direction={variant === "inline" ? "row" : "column"}
             alignItems={position}
             sx={{
-                py: "45px",
+                py: variant === "inline" ? "10px" : "45px",
             }}
         >
-            <CircularProgress {...resolvedColor} />
+            <CircularProgress {...resolvedColor} size={resolvedSize} thickness={5} />
             {label && (
-                <Typography variant="body1" component="div" mt={2}>
+                <Typography
+                    variant="body1"
+                    color="#333"
+                    fontStyle="italic"
+                    component={variant === "inline" ? "span" : "div"}
+                    pl={variant === "inline" ? "10px" : 0}
+                    mt={variant === "inline" ? 0 : 2}
+                >
                     {label}
                 </Typography>
             )}
